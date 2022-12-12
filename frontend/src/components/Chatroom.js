286b3a42612a1fe0ff9d953c48be4334834daa75
Mohammad55666 <55007814+Mohammad5566@users.noreply.github.com>
2022-12-11 23:10:57 -0500

    Integrating front end with back end

--- frontend/src/components/Chatroom.js
+++ frontend/src/components/Chatroom.js
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
 import {
   Box,
+  List,
   Text,
   Avatar,
   VStack,
   HStack,
+  ListItem,
+  ListIcon,
+  OrderedList,
+  UnorderedList,
   useColorModeValue,
 } from "@chakra-ui/react";
 
@@ -16,18 +22,54 @@ function Chatroom() {
     </HStack>
   );
 
-  const chatBox = <></>;
-
-  return (
-    <Box
+  const chatBox = (
+    <VStack
       margin="10% 30% 10% 30%"
       boxShadow="sm"
       rounded="md"
       bg={useColorModeValue("gray.100", "gray.900")}
-    >
-      {userInfo}
-    </Box>
+    ></VStack>
+  );
+
+  let messageList = [];
+  const [input, setInput] = useState("");
+
+  const socket = io.connect(`http://localhost:3001`);
+  const sendMessage = () => {
+    socket.emit("chat message", socket.id, "TEST MESSAGE");
+  };
+
+  useEffect(() => {
+    socket.on("chat message", (id, msg) => {
+      var item = document.createElement("li");
+      item.textContent = `${id} wrote: ${msg}`;
+      messageList.appendChild(
+        <ListItem>
+          `${id} wrote: ${msg}`;
+        </ListItem>
+      );
+      window.scrollTo(0, document.body.scrollHeight);
+    });
+  }, [socket, messageList]);
+
+  return (
+    <>
+      {console.log(messageList)}
+      <UnorderedList id="message-list">{messageList}</UnorderedList>
+      <form id="form" action="">
+        <input id="input" autocomplete="off" />
+        <button onClick={sendMessage}>Send</button>
+      </form>
+    </>
   );
 }
+
+//   margin="10% 30% 10% 30%"
+//   boxShadow="sm"
+//   rounded="md"
+//   bg={useColorModeValue("gray.100", "gray.900")}
+// >
+//   {userInfo}
+//
 
 export default Chatroom;
